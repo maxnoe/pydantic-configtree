e5266b7389f7771f5c2cafc68564d6254f7785fd
Maximilian Linhoff <maximilian.linhoff@cta-observatory.org>
2025-08-18 12:17:17 +0200

    Add more test

--- src/pydantic_settings_ctapipe/tests/test_astropy.py
+++ src/pydantic_settings_ctapipe/tests/test_astropy.py
@@ -1,9 +1,11 @@
+import json
+
 import pytest
 from astropy.time import Time
-from pydantic import TypeAdapter, ValidationError
+from pydantic import BaseModel, TypeAdapter, ValidationError
 
 
-def test_time():
+def test_time_typeadapter():
     from pydantic_settings_ctapipe.astropy import AstropyTime
 
     ta = TypeAdapter(AstropyTime)
@@ -20,3 +22,21 @@ def test_time():
 
     with pytest.raises(ValidationError):
         ta.validate_json("1755511106.116")
+
+
+def test_time_model():
+    from pydantic_settings_ctapipe.astropy import AstropyTime
+
+    class HasTime(BaseModel):
+        timestamp: AstropyTime
+
+    t = Time.now()
+    data = HasTime(timestamp=t)
+    assert data.timestamp == t
+
+    # check that python model dump still contains astropy time
+    assert data.model_dump()["timestamp"] == t
+
+    # json should store ISOT UTC string
+    parsed = json.loads(data.model_dump_json())
+    assert parsed["timestamp"] == Time(t, precision=9).utc.isot
